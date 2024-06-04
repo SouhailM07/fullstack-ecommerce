9371d14e1f0cabef14368df8478bb19cbe858446
shadow <lightshadow416@gmail.com>
2024-06-04 17:16:35 +0200

    [Hero] component checkpoint-lg

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,33 @@
 import "./hero.css";
 export default function Hero() {
-  return <>Hero</>;
+  return (
+    <section className="cc px-[1rem] grid grid-cols-2  gap-x-[1rem]">
+      <article className="space-y-[2rem]">
+        <h1 className="text-[3rem] font-bold leading-[3.2rem]">
+          Discover the Perfect Piece
+        </h1>
+        <p>
+          Explore out curated collection of high-quality , stylish products that
+          will elevate your everyday life.
+        </p>
+        <ul role="list" className="space-x-[2rem]">
+          <MyBtn txt="Shop Now" color="bg-slate-900 text-white" />
+          <MyBtn txt="Learn More" color="border-black border" />
+        </ul>
+      </article>
+      <article>
+        <img
+          src="https://placehold.co/600x400/png"
+          alt="img"
+          className="w-full"
+        />
+      </article>
+    </section>
+  );
 }
+
+const MyBtn = ({ color, txt }: { color: string; txt: string }) => (
+  <button role="listitem" className={`${color} h-[3rem] w-[9rem] rounded-lg`}>
+    {txt}
+  </button>
+);
