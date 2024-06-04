c2f66dba6fcdf921eeca853d5815b1f17938dfdf
shadow <lightshadow416@gmail.com>
2024-06-04 17:29:02 +0200

    [Categories] component checkpoint-lg

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,4 +1,32 @@
 import "./categories.css";
+
+import { motion } from "framer-motion";
+
 export default function Categories() {
-  return <>Categories</>;
+  const categories: string[] = [
+    "Clothing",
+    "Accessories",
+    "Home",
+    "Tech",
+    "Beauty",
+  ];
+  return (
+    <section className="bg-gray-100 cc px-[1rem] h-[20rem] flex items-center">
+      <ul className="grid grid-cols-5 gap-x-[1rem] w-full">
+        {categories.map((e, i) => (
+          <Category key={i} txt={e} />
+        ))}
+      </ul>
+    </section>
+  );
 }
+
+const Category = ({ txt }: { txt: string }) => (
+  <motion.button
+    whileHover={{ y: -15 }}
+    transition={{ ease: "anticipate", duration: 0.1 }}
+    className="bg-gray-300 shadow-xl h-[8rem] font-bold rounded-lg w-full "
+  >
+    {txt}
+  </motion.button>
+);
--- src/components/MyContainer/MyContainer.tsx
+++ src/components/MyContainer/MyContainer.tsx
@@ -1,12 +1,13 @@
 import "./mycontainer.css";
 // components
-import { Navbar, Hero } from "@/components";
+import { Navbar, Hero, Categories } from "@/components";
 export default function MyContainer() {
   return (
     <>
       <Navbar />
-      <main className="mt-[4rem]">
+      <main className="mt-[4rem] space-y-[7rem]">
         <Hero />
+        <Categories />
       </main>
     </>
   );
