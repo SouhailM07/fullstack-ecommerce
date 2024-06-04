202ccc3178dfd4354623e921221c7a6888f27214
shadow <lightshadow416@gmail.com>
2024-06-04 17:17:28 +0200

    [Categories] component was created

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,4 @@
+import "./categories.css";
+export default function Categories() {
+  return <>Categories</>;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as MyContainer } from "./MyContainer/MyContainer";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Hero } from "./Hero/Hero";
+export { default as Categories } from "./Categories/Categories";
