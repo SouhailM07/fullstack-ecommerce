a3ed52d4dac28c62ac0bb9ccb1c3060476ea7900
shadow <lightshadow416@gmail.com>
2024-06-05 16:17:40 +0200

    [Footer] component was created

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Hero } from "./Hero/Hero";
 export { default as Categories } from "./Categories/Categories";
 export { default as FeaturedProducts } from "./FeaturedProducts/FeaturedProducts";
+export { default as Footer } from "./Footer/Footer";
