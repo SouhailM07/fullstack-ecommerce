500c061e801f690a399915db75adae0ba59028a6
shadow <lightshadow416@gmail.com>
2024-06-10 16:00:43 +0200

    [Footer] component is now /RESPONSIVE/

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -24,30 +24,28 @@ export default function Footer() {
   return (
     <footer className=" bg-slate-900 text-white">
       <section className="cc px-[1rem] py-[2rem] space-y-[3rem]">
-        <article>
-          <ul className="grid grid-cols-4 gap-x-[1rem]">
-            {links.map(({ title, txt }, i) => (
-              <li key={i} className="space-y-[1rem]">
-                <p className="text-[0.9rem] font-bold">{title}</p>
-                <p className="text-[0.8rem]">{txt}</p>
-              </li>
-            ))}
-            <li className="space-y-[1rem] mx-auto">
-              <p className="text-[0.9rem] font-bold">Follow Us</p>
-              <ul role="list" className="flex gap-x-[1.3rem]">
-                {socialLinks.map((e, i) => (
-                  <FontAwesomeIcon
-                    role="listitem"
-                    key={i}
-                    icon={e.icon}
-                    aria-label={e.label}
-                  />
-                ))}
-              </ul>
+        <ul className="grid grid-cols-4 gap-x-[1rem] max-lg:grid-cols-2 gap-y-[1.5rem] max-md:grid-cols-1  ">
+          {links.map(({ title, txt }, i) => (
+            <li key={i} className="space-y-[1rem]">
+              <p className="text-[0.9rem] font-bold">{title}</p>
+              <p className="text-[0.8rem]">{txt}</p>
             </li>
-          </ul>
-        </article>
-        <article className="text-center text-[0.9rem] border-gray-500 border-t pt-[1.5rem]">
+          ))}
+          <li className="space-y-[1rem] lg:mx-auto">
+            <p className="text-[0.9rem] font-bold">Follow Us</p>
+            <ul role="list" className="flex gap-x-[1.3rem]">
+              {socialLinks.map((e, i) => (
+                <FontAwesomeIcon
+                  role="listitem"
+                  key={i}
+                  icon={e.icon}
+                  aria-label={e.label}
+                />
+              ))}
+            </ul>
+          </li>
+        </ul>
+        <article className="text-center text-[0.9rem] border-gray-500 border-t pt-[1.5rem] ">
           @2024 Acne Ecommerce. All rights reserved.
         </article>
       </section>
